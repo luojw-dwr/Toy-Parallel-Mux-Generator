47eba10218ce0c19040bb2aca1138ca35cf6c128
luojw <luojw@shanghaitech.edu.cn>
2020-09-11 18:50:30 +0800

    added comments into the generators

--- src/main/scala/muxgen/muxgen.scala
+++ src/main/scala/muxgen/muxgen.scala
@@ -3,7 +3,18 @@ package muxgen
 import chisel3._
 import chisel3.util.log2Up
 
+/*
+* Method object to generate a mux with its number of inputs a power of 2.
+*/
 object MuxPow2 {
+    /*
+    * Returns the output of the output of the mux.
+    * The number of inputs should be a power of 2.
+    * @T:   input type
+    * @gen: indicator of input data type
+    * @sel: selection signal
+    * @xs:  vector of inputs
+    */
     def apply[T <: Data](gen: T, sel: UInt, xs: Seq[T]): T = {
         val rank = sel.getWidth
         val n = 1 << rank
@@ -11,19 +22,38 @@ object MuxPow2 {
         if (rank == 1) {
             Mux2(gen, sel, xs)
         } else {
+            // sel for the final input
             val sel_MSB = sel(rank - 1)
+            // sel for the smaller muxes
             val sel_LBS = sel(rank - 2, 0)
+            // inputs within high range
             val xsG = ((n >> 1) until n).map((idx: Int) => xs(idx))
+            // inputs within low range
             val xsL = (0 until (n >> 1)).map((idx: Int) => xs(idx))
+            // smaller mux for high range
             val muxG = MuxPow2(gen, sel_LBS, xsG)
+            // smaller mux for low range
             val muxL = MuxPow2(gen, sel_LBS, xsL)
+            // mux2 for the final output
             Mux2(gen, sel_MSB, VecInit(muxL, muxG))
         }
     }
 }
 
+/*
+* Method object to generate a mux with its number of inputs larger than 1.
+*/
 object MuxAny {
-    def apply[T <: Data](gen: T, n: Int, sel: UInt, xs: Seq[T]): T = {
+    /*
+    * Returns the output of the mux.
+    * The number of inputs should be larger than 1.
+    * @T:   input type
+    * @gen: indicator of input data type
+    * @sel: selection signal
+    * @xs:  vector of inputs
+    */
+    def apply[T <: Data](gen: T, sel: UInt, xs: Seq[T]): T = {
+        val n = xs.length
         assert(n > 1)
         val rankCeil = log2Up(n)
         assert(sel.getWidth >= rankCeil)
@@ -32,27 +62,48 @@ object MuxAny {
             MuxPow2(gen, sel, xs)
         } else {
             val rankFloor = rankCeil - 1
+            // number of inputs which lie in low range
             val nL = 1 << rankFloor
+            // number of inputs which lie in high range
             val nG = n - nL
+            // sel for the final input
             val sel_MSB = sel(rankCeil - 1)
+            // sel for low range
             val sel_LBG = sel(log2Up(nG) - 1, 0)
+            // sel for high range
             val sel_LBL = sel(rankFloor - 1, 0)
+            // inputs within high range
             val xsG = (nL until n).map((idx: Int) => xs(idx))
+            // inputs within low range
             val xsL = (0 until nL).map((idx: Int) => xs(idx))
+            // use the input directly if only one input in high range,
+            // otherwise generates a smaller mux for high range
             val yG = if (nG == 1) {
+                // when the number of inputs in high range is 1,
+                // use the input directly and finish recursion
                 xs(n - 1)
             } else {
-                MuxAny(gen, nG, sel_LBG, xsG)
+                MuxAny(gen, sel_LBG, xsG)
             }
-            val yL = MuxAny(gen, nL, sel_LBL, xsL)
+            // smaller mux for low range
+            val yL = MuxAny(gen, sel_LBL, xsL)
+            // mux2 for the final output
             Mux2(gen, sel_MSB, VecInit(yL, yG))
         }
     }
 }
 
+
+/*
+* Mux module for any given number of inputs
+* @T:   input type
+* @gen: indicator of input data type
+* @n:   number of inputs
+* @io:  MuxIO[T](n)
+*/
 class MuxN[T <: Data](private val gen: T, val n: Int) extends Module {
     assert(n > 1)
     override def desiredName = s"Mux${n.toString}"
     val io = IO(MuxIO(gen, n))
-    io.y := MuxAny(gen, n, io.sel, io.xs)
+    io.y := MuxAny(gen, io.sel, io.xs)
 }
--- src/main/scala/muxgen/namedmuxgen.scala
+++ src/main/scala/muxgen/namedmuxgen.scala
@@ -3,29 +3,52 @@ package muxgen
 import chisel3._
 import chisel3.util.log2Up
 
+/*
+* NamedMux module for any given number of inputs
+* @T:    input type
+* @NNat: number of inputs coded in type system
+* @gen:  indicator of input data type
+* @nat:  indicator of number of inputs
+* @io:   MuxIO[T](n)
+*/
 class NamedMuxN[T <: Data, NNat <: Nat](private val gen: T, val nat: NNat) extends Module {
     val n = nat.asInt
     assert(n > 1)
     override def desiredName = s"NamedMux${n.toString}"
     val io = IO(MuxIO(gen, n))
     val selWidth = io.sel.getWidth
-    val xss = NamedMuxN.decomposeXs(io.xs)
+    // the number of inputs in low range
+    val nL = (1 << (log2Up(n) - 1))
+    // groups the inputs into those in low range and those in high range
+    val xss = io.xs.grouped(nL).toSeq
+    // generate two smalled NamesMuxes for the two ranges
     val ys = xss.map((xs: Seq[T]) => NamedMuxN(gen, io.sel(selWidth - 2, 0), xs))
+    // generate a mux2 for the final output
     io.y := Mux2(gen, io.sel(selWidth - 1), ys)
 }
 
+
+/*
+* Method object for the logic to generate a NamedMux
+*/
 object NamedMuxN {
-    def decomposeXs[T <: Data](xs: Seq[T]) = {
-        val n = xs.length
-        val nL = (1 << (log2Up(n) - 1))
-        xs.grouped(nL).toSeq
-    }
+    /*
+    * Returns the output of the NamedMux.
+    * The number of inputs must be larger than 0.
+    * @T:   input type
+    * @gen: indicator of input data type
+    * @sel: selection signal
+    * @xs:  vector of inputs
+    */
     def apply[T <: Data](gen: T, sel: UInt, xs: Seq[T]): T = {
         val n = xs.length
         assert(n > 0)
         n match {
+            // when n is 1, use the input directly and finish recursion
             case 1 => xs(0)
+            // when n is 2, generate a mux2 and finish recursion
             case 2 => Mux2(gen, sel, xs)
+            // otherwise, continue recursion by generating a smaller NamedMuX
             case _ => {
                 val m = Module(new NamedMuxN(gen, Nat(n))).io
                 m.sel := sel
